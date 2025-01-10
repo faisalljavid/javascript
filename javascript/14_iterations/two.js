7633932a81e5c2081ba31923868e20f1df2d3a0d
Faisal Javid <faisalljavid@gmail.com>
2025-01-10 23:31:00 +0530

    Iterations [half]

---
+++ javascript/14_iterations/two.js
@@ -0,0 +1,28 @@
+// while
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2; 
+}
+
+
+
+let myArray = ["flash", "batman", "superman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+    
+}
+
+
+
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`); // output = 11 'only'
+    score++;
+} while (score <= 10); 
